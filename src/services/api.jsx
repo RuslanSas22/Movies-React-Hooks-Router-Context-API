4b69fd92b0760044301a688dffd0ee67104446ca
“Ruslan <ruslansas22@gmail.comgit config --global user.emailgit config --global user.email ruslansas22@gmail.com>
2026-04-29 18:33:50 +0300

    first commit

---
+++ src/services/api.jsx
@@ -0,0 +1,19 @@
+const API_KEY = "eaed6afbd24dae5e5c96f15b189e00a8";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query,
+    )}`,
+  );
+
+  const data = await response.json();
+  return data.results;
+};
